ac566bb2d5e658c4e01f077ff22ea00b230cd28c
akuma <akumahoneko@gmail.com>
2024-12-14 13:38:23 +0700

    - Hopefully fixed start-up crash

--- src/main/java/com/yukami/efwingscompat/EFWingsCompat.java
+++ src/main/java/com/yukami/efwingscompat/EFWingsCompat.java
@@ -13,8 +13,9 @@ public class EFWingsCompat
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "efwingscompat";
-    public EFWingsCompat(FMLJavaModLoadingContext context)
+    public EFWingsCompat()
     {
+        FMLJavaModLoadingContext context = FMLJavaModLoadingContext.get();
         IEventBus bus = context.getModEventBus();
         bus.addListener(Animation::registerAnimations);
         LivingMotion.ENUM_MANAGER.registerEnumCls(EFWingsCompat.MODID, EFLivingMotions.class);
